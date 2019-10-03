8dc2145ab97a17d80b919cc58026b068d7142dad
JSalicram <JSalicram@users.github.com>
2019-10-03 11:42:39 +0100

    Initial commit

---
+++ Assets/Editor/SelectAllOfTag.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SelectAllOfTag : ScriptableWizard
+{
+    public string searchTag = "your tag here...";
+
+    [MenuItem ("My Tools/Select all of tag...")]
+    static void SelectAllOfWizard()
+    {
+        ScriptableWizard.DisplayWizard<SelectAllOfTag>("Select All Of Tag...", "Make Selection");
+    }
+
+    private void OnWizardCreate()
+    {
+        GameObject[] gameobjects = GameObject.FindGameObjectsWithTag(searchTag);
+        Selection.objects = gameobjects;
+    }
+}
---
+++ Assets/Editor/SpriteProccessor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SpriteProccessor : AssetPostprocessor
+{
+    private void OnPostprocessTexture(Texture2D texture)
+    {
+        Debug.Log("Texture imported");
+        string lowerCaseAssetPath = assetPath.ToLower();
+        bool isInSpriteDirectory = lowerCaseAssetPath.IndexOf("/sprites/") != -1;
+
+        if (isInSpriteDirectory)
+        {
+            Debug.Log("Sprite in directory");
+            TextureImporter textureImporter = (TextureImporter)assetImporter;
+            textureImporter.textureType = TextureImporterType.Sprite;
+
+            //start of assessment
+            textureImporter.maxTextureSize = 1024;
+            textureImporter.crunchedCompression = true;
+            textureImporter.textureCompression = TextureImporterCompression.CompressedHQ;
+            textureImporter.spritePixelsPerUnit = 50;
+
+            /*
+            TextureImporterPlatformSettings textureImporterPlatform = TextureImporterPlatformSettings;
+            textureImporterPlatform.resizeAlgorithm = TextureResizeAlgorithm.Bilinear;*/
+        }
+    }
+}
